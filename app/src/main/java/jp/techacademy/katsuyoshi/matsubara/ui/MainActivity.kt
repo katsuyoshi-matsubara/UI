8c20a31a80680978112ecbe18ebac78016e1a091
katsuyoshi-matsubara <unikamonec@gmail.com>
2020-03-22 14:56:23 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/katsuyoshi/matsubara/ui/MainActivity.kt
@@ -0,0 +1,24 @@
+package jp.techacademy.katsuyoshi.matsubara.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import android.util.Log
+import android.view.View
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener {
+            textView.text = editText.text.toString()
+        }
+
+    }
+
+
+}
